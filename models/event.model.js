d87a7fa2bfd728dfb042c2fd7dca04469f65f66b
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-29 17:17:47 -0300

    Se configuro el modelo de Evento con su respectiva relacion con la base de datos

---
+++ models/event.model.js
@@ -0,0 +1,76 @@
+const db = require('../config/db.config');
+
+const Event = {};
+
+Event.create = (eventData, result) => {
+  db.query("INSERT INTO events SET ?", eventData, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    result(null, { id: res.insertId, ...eventData });
+  });
+};
+
+Event.findById = (id, result) => {
+  db.query(`SELECT * FROM events WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    if (res.length) {
+      result(null, res[0]);
+      return;
+    }
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Event.getAll = (result) => {
+  db.query("SELECT * FROM events", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    result(null, res);
+  });
+};
+
+Event.updateById = (id, event, result) => {
+  db.query(
+    "UPDATE events SET titulo = ?, categoria = ?, fechaHora = ?, ubicacion = ?, descripcion = ?, precio = ?, imagen = ? WHERE id = ?",
+    [event.titulo, event.categoria, event.fechaHora, event.ubicacion, event.descripcion, event.precio, event.imagen, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      result(null, { id: id, ...event });
+    }
+  );
+};
+
+Event.remove = (id, result) => {
+  db.query("DELETE FROM events WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+    result(null, res);
+  });
+};
+
+module.exports = Event;
