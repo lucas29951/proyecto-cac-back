41ff8bc5ae1b4fbfe2f06221df87b1c66af107ef
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-29 17:11:43 -0300

    Se configuro la conexion a la base de datos

---
+++ config/db.config.js
@@ -0,0 +1,17 @@
+
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'eventickets_db',
+    port: process.env.DB_PORT || 3308,
+});
+
+db.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
+
+module.exports = db;
