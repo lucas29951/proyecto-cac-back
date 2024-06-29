67158832636b0f65e2ac569ed16dbcc16f337625
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-29 17:14:32 -0300

    Se configuraron las opciones CRUD de eventos

---
+++ controllers/events.controller.js
@@ -0,0 +1,89 @@
+const Event = require('../models/event.model');
+
+exports.create = (req, res) => {
+  const newEvent = {
+    titulo: req.body.titulo,
+    categoria: req.body.categoria,
+    fechaHora: req.body.fechaHora,
+    ubicacion: req.body.ubicacion,
+    descripcion: req.body.descripcion,
+    precio: req.body.precio,
+    imagen: req.body.imagen
+  };
+
+  Event.create(newEvent, (err, data) => {
+    if (err) {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Event."
+      });
+    } else res.send(data);
+  });
+};
+
+exports.findAll = (req, res) => {
+  Event.getAll((err, data) => {
+    if (err) {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving events."
+      });
+    } else res.send(data);
+  });
+};
+
+exports.findOne = (req, res) => {
+  Event.findById(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Event with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Event with id " + req.params.id
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+exports.update = (req, res) => {
+  const event = {
+    titulo: req.body.titulo,
+    categoria: req.body.categoria,
+    fechaHora: req.body.fechaHora,
+    ubicacion: req.body.ubicacion,
+    descripcion: req.body.descripcion,
+    precio: req.body.precio,
+    imagen: req.body.imagen
+  };
+
+  Event.updateById(req.params.id, event, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Event with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Error updating Event with id " + req.params.id
+        });
+      }
+    } else res.send(data);
+  });
+};
+
+exports.delete = (req, res) => {
+  Event.remove(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Event with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Event with id " + req.params.id
+        });
+      }
+    } else res.send({ message: `Event was deleted successfully!` });
+  });
+};
