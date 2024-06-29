99a5aaa557af503134fa9c638a210ebcd0e83196
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-29 17:16:13 -0300

    Se configuraron las validaciones de token

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  let token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  token = token.replace('Bearer ', '');
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+module.exports = {
+  verifyToken
+};
