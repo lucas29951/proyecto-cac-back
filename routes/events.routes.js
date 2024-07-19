8deee7c73dbf9dff203300271d05dc8db87631f5
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-19 15:18:13 -0300

    Modificacion de las rutas de eventos

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const eventsController = require('../controllers/events.controller');
 const authMiddleware = require('../middleware/auth.middleware');
-const upload = require('../config/multer.config');
 
-router.post('/', authMiddleware.verifyToken, upload.single('imagen'), eventsController.create);
+router.post('/', authMiddleware.verifyToken, eventsController.create);
 router.get('/', eventsController.findAll);
 router.get('/:id', eventsController.findOne);
-router.put('/:id', authMiddleware.verifyToken, upload.single('imagen'), eventsController.update);
+router.put('/:id', authMiddleware.verifyToken, eventsController.update);
 router.delete('/:id', authMiddleware.verifyToken, eventsController.delete);
 
 module.exports = router;
